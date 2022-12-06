cb5670b340db9ca45c0ccaf319bdb07aac3fc3e0
lampubohlam <116137169+lampubohlam@users.noreply.github.com>
2022-12-06 19:48:51 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+print ("NAMA : Abdul aziz")
+print("NIM : 312210022")
+print(30*"=")
+import math
+def a(x):
+    return x**2
+    a = lambda x: x**2, a
+print(a(2))
+
+def b(x, y):
+    return math.sqrt(x*2 + y*2)
+    b = lambda x, y: x*2 + y*2, b
+print(b(2, 4))
+
+def c(*args):
+    return sum(args) / len(args)
+    c = lambda*args: sum(args) / len(args)
+print(c(10, 50))
+
+def d(s):
+    return "".join(set(s))
+    d = lambda s: "".join(set(s))
